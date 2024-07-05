bcd2d6a0abe28508abbdf4bf5584e11893cbb0cd
RomwelFeliciano <romfeliciano18@gmail.com>
2024-07-05 13:34:07 +0800

    Feature Complete

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,16 +1,16 @@
-import { useCities } from "../contexts/CitiesContext";
-import styles from "./CityItem.module.css";
-import { Link } from "react-router-dom";
+import { useCities } from '../contexts/CitiesContext';
+import styles from './CityItem.module.css';
+import { Link } from 'react-router-dom';
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
 
   const {
     cityName,
@@ -20,16 +20,23 @@ function CityItem({ city }) {
     position: { lat, lng },
   } = city;
 
+  function handleDelete(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
+
   return (
     <li>
       <Link
-        className={`${styles.cityItem} ${id === currentCity.id ? styles["cityItem--active"] : ""}`}
+        className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : ''}`}
         to={`${id}?lat=${lat}&lng=${lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleDelete}>
+          &times;
+        </button>
       </Link>
     </li>
   );
